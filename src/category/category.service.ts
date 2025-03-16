5640ac9ae82e5d0a546200ad2e306024838ac91b
Javohir11011 <ajavohir545@gmail.com>
2025-03-16 20:33:03 +0500

    added category success

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,26 +1,73 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CategoryEntity } from './entities/category.entity';
 
 @Injectable()
 export class CategoryService {
-  create(createCategoryDto: CreateCategoryDto) {
-    return 'This action adds a new category';
+  constructor(
+    @InjectRepository(CategoryEntity)
+    private readonly repo: Repository<CategoryEntity>,
+  ) {}
+
+  async create(createCategoryDto: CreateCategoryDto) {
+    const oldCategory = await this.repo.findOneBy({
+      name: createCategoryDto.name,
+    });
+
+    if (oldCategory) {
+      throw new ConflictException('Name already exisist');
+    }
+
+    return await this.repo.save(createCategoryDto);
   }
 
-  findAll() {
-    return `This action returns all category`;
+  async findAll() {
+    return this.repo.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} category`;
+  async findOne(id: string) {
+    const category = await this.repo.findOneBy({ id });
+    if (!category) {
+      throw new NotFoundException('Not found');
+    }
+    return category;
   }
 
-  update(id: number, updateCategoryDto: UpdateCategoryDto) {
-    return `This action updates a #${id} category`;
+  async update(id: string, updateCategoryDto: UpdateCategoryDto) {
+    const category = await this.repo.findOneBy({ id });
+    if (!category) {
+      throw new NotFoundException('Not found');
+    }
+    if (updateCategoryDto?.name) {
+      const oldCategory = await this.repo.findOneBy({
+        name: updateCategoryDto.name,
+      });
+      if (oldCategory) {
+        throw new ConflictException('Name already exisist');
+      }
+    }
+    await this.repo.update(id, updateCategoryDto);
+
+    // Object.assign(category, updateCategoryDto);
+    return { ...category, ...updateCategoryDto };
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} category`;
+  async remove(id: string) {
+    const category = await this.repo.findOneBy({ id });
+    if (!category) {
+      throw new NotFoundException('Not found');
+    }
+    await this.repo.delete(id);
+
+    return {
+      message: 'success',
+    };
   }
 }
